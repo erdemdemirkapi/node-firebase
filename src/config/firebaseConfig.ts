362675e2886bae26ef25e7b01622ce4fd1ed32b7
Erdem Demirkapı <demirkapierdem@gmail.com>
2024-10-22 10:54:33 +0300

    Update firebase config

--- src/config/firebaseConfig.ts
+++ src/config/firebaseConfig.ts
@@ -3,11 +3,13 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 
-const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY || '{}');
-
 admin.initializeApp({
-    credential: admin.credential.cert(serviceAccount as admin.ServiceAccount),
-    databaseURL: process.env.FIREBASE_DATABASE_URL,
-});
+    credential: admin.credential.cert({
+        projectId: process.env.FIREBASE_PROJECT_ID,
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n')
+    }),
+    databaseURL: process.env.FIREBASE_DATABASE_URL
+  });
 
 export { admin };
